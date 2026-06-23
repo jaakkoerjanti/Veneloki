7e5099bb3368af51ff26f379337baa7f346fcd1a
jaakkoerjanti <jaakko@erjanti.fi>
2026-06-23 13:23:10 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Veneloki – service worker
 // Tallentaa sovelluksen tiedostot laitteelle, jotta se toimii ilman nettiä.
 // Nosta versionumeroa aina kun päivität sovellusta, niin vanha välimuisti vaihtuu.
-const CACHE = "veneloki-v17";
+const CACHE = "veneloki-v18";
 
 const ASSETS = [
   "./",
